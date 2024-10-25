c687ed1cd80d404b704fa44040cb09673085d800
Wojciech Zbrożek <wojciech.zbrozek@gmail.com>
2024-10-25 15:55:20 +0200

    dropdown component improved

--- dist/index.js
+++ dist/index.js
@@ -1,38 +1,56 @@
 "use strict";
 function switchTab(event) {
-    const target = event.target;
-    const tabButtons = document.querySelectorAll('[role="tab"]');
-    const tabPanels = document.querySelectorAll('[role="tabpanel"]');
-    const selectedTabId = target.getAttribute('aria-controls') || '';
-    tabButtons.forEach(button => {
-        button.setAttribute('data-active', 'false');
-    });
-    tabPanels.forEach(panel => {
-        panel.hidden = true;
-    });
-    target.setAttribute('data-active', 'true');
-    const selectedTabPanel = document.getElementById(selectedTabId);
-    if (selectedTabPanel) {
-        selectedTabPanel.hidden = false;
-    }
+  const target = event.target;
+  const tabButtons = document.querySelectorAll('[role="tab"]');
+  const tabPanels = document.querySelectorAll('[role="tabpanel"]');
+  const selectedTabId = target.getAttribute("aria-controls") || "";
+  tabButtons.forEach((button) => {
+    button.setAttribute("data-active", "false");
+  });
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
+  target.setAttribute("data-active", "true");
+  const selectedTabPanel = document.getElementById(selectedTabId);
+  if (selectedTabPanel) {
+    selectedTabPanel.hidden = false;
+  }
 }
-document.querySelectorAll('[role="tab"]').forEach(button => {
-    button.addEventListener('click', switchTab);
+document.querySelectorAll('[role="tab"]').forEach((button) => {
+  button.addEventListener("click", switchTab);
 });
-const select = document.querySelector('.custom-dropdown');
-if (select) {
-    select.addEventListener('click', () => {
-        select.classList.toggle('expanded');
-    });
+const select = document.querySelectorAll(".custom-dropdown");
+if (select.length) {
+  select.forEach((elem) =>
+    elem.addEventListener("click", () => {
+      elem.classList.toggle("expanded");
+    })
+  );
 }
-const dropdownItems = document.querySelectorAll('.http-method .dropdown-list .dropdown-item');
-const chosen = document.querySelector('.http-method .chosen');
-if (chosen) {
-    dropdownItems.forEach((item) => {
-        item.addEventListener('click', () => {
-            chosen.textContent = item.textContent;
-            dropdownItems.forEach(item => item.classList.remove('selected'));
-            item.classList.add('selected');
-        });
+const httpMethodDropdownItems = document.querySelectorAll(
+  ".http-method .dropdown-list .dropdown-item"
+);
+const httpMethodChosen = document.querySelector(".http-method .chosen");
+if (httpMethodChosen) {
+    httpMethodDropdownItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        httpMethodChosen.textContent = item.textContent;
+        httpMethodDropdownItems.forEach((item) => item.classList.remove("selected"));
+      item.classList.add("selected");
     });
+  });
 }
+
+const reqBodyTypeDropdownItems = document.querySelectorAll(
+    ".req-body-type .dropdown-list .dropdown-item"
+  );
+  const reqBodyTypeChosen = document.querySelector(".req-body-type .chosen");
+  if (reqBodyTypeChosen) {
+    reqBodyTypeDropdownItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        reqBodyTypeChosen.textContent = item.textContent;
+        reqBodyTypeDropdownItems.forEach((item) => item.classList.remove("selected"));
+        item.classList.add("selected");
+      });
+    });
+  }
--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,58 @@
-function switchTab(event: Event): void {
-    const target = event.target as HTMLButtonElement;
-    const tabButtons = document.querySelectorAll<HTMLButtonElement>('[role="tab"]');
-    const tabPanels = document.querySelectorAll<HTMLDivElement>('[role="tabpanel"]');
-    const selectedTabId = target.getAttribute('aria-controls') || '';
+function switchTab(event: Event) {
+  const target = event.target as HTMLElement;
+  const tabButtons = document.querySelectorAll<HTMLElement>('[role="tab"]');
+  const tabPanels = document.querySelectorAll<HTMLElement>('[role="tabpanel"]');
+  const selectedTabId = target.getAttribute("aria-controls") || "";
 
-    tabButtons.forEach(button => {
-        button.setAttribute('data-active', 'false');
-    });
+  tabButtons.forEach((button) => {
+    button.setAttribute("data-active", "false");
+  });
 
-    tabPanels.forEach(panel => {
-        panel.hidden = true;
-    });
+  tabPanels.forEach((panel) => {
+    panel.hidden = true;
+  });
 
-    target.setAttribute('data-active', 'true');
-    const selectedTabPanel = document.getElementById(selectedTabId) as HTMLDivElement;
-    if (selectedTabPanel) {
-        selectedTabPanel.hidden = false;
-    }
+  target.setAttribute("data-active", "true");
+
+  const selectedTabPanel = document.getElementById(selectedTabId);
+  if (selectedTabPanel) {
+    selectedTabPanel.hidden = false;
+  }
 }
 
-document.querySelectorAll<HTMLButtonElement>('[role="tab"]').forEach(button => {
-    button.addEventListener('click', switchTab);
+document.querySelectorAll<HTMLElement>('[role="tab"]').forEach((button) => {
+  button.addEventListener("click", switchTab);
 });
 
-
-const select = document.querySelector<HTMLDivElement>('.custom-dropdown');
-
-if (select) {
-    select.addEventListener('click', () => {
-        select.classList.toggle('expanded');
+const customDropdowns = document.querySelectorAll<HTMLElement>(".custom-dropdown");
+customDropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", () => {
+    dropdown.classList.toggle("expanded");
   });
-}
+});
 
-const dropdownItems = document.querySelectorAll<HTMLDivElement>('.http-method .dropdown-list .dropdown-item');
-const chosen = document.querySelector<HTMLDivElement>('.http-method .chosen');
+const httpMethodDropdownItems = document.querySelectorAll<HTMLElement>(".http-method .dropdown-list .dropdown-item");
+const httpMethodChosen = document.querySelector<HTMLElement>(".http-method .chosen");
 
-if (chosen) {
-  dropdownItems.forEach((item) => {
-    item.addEventListener('click', () => {
-      chosen.textContent = item.textContent;
-      dropdownItems.forEach(item => item.classList.remove('selected'))
-      item.classList.add('selected');
+if (httpMethodChosen) {
+  httpMethodDropdownItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      httpMethodChosen.textContent = item.textContent;
+      httpMethodDropdownItems.forEach((item) => item.classList.remove("selected"));
+      item.classList.add("selected");
     });
   });
 }
 
+const reqBodyTypeDropdownItems = document.querySelectorAll<HTMLElement>(".req-body-type .dropdown-list .dropdown-item");
+const reqBodyTypeChosen = document.querySelector<HTMLElement>(".req-body-type .chosen");
+
+if (reqBodyTypeChosen) {
+  reqBodyTypeDropdownItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      reqBodyTypeChosen.textContent = item.textContent;
+      reqBodyTypeDropdownItems.forEach((item) => item.classList.remove("selected"));
+      item.classList.add("selected");
+    });
+  });
+}
